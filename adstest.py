ec8276b79f717961665ca34a64ae4542bc663a45
hvzr <68715818+hvzr@users.noreply.github.com>
2020-08-27 19:31:52 -0400

    Update adstest.py

--- adstest.py
+++ adstest.py
@@ -18,34 +18,39 @@ chrome_options = Options()
 chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get("https://www.nytimes.com/2020/08/13/us/politics/trump-israel-united-arab-emirates-uae.html")
+driver.get("https://www.npr.org/sections/live-updates-protests-for-racial-justice/2020/08/18/903478960/st-louis-couple-who-brandished-guns-at-black-protesters-to-speak-at-rnc")
 driver.maximize_window()
 
 urldict = {}
 
-t_end = time.time() + 60 * 20
-while time.time() < t_end:
-    time.sleep(10)
-    screenWidth, screenHeight = pyautogui.size()    # Macbook pro 13" is 1440x900
-    targetposx = screenWidth/2
-    targetposy = 250
-    pyautogui.click(targetposx,targetposy)
-    time.sleep(10)
-    try:
-        driver.switch_to.window(driver.window_handles[1])
-        url = driver.current_url
-        url = urlsplit(url)[1]
-        if url in urldict: #counts instances of urls
-            urldict[url] += 1
-        else:
-            urldict[url] = 1
-        while len(driver.window_handles) > 1: #fixes extra tab issue
+t_end = time.time() + 60 * 10
+try:
+    while time.time() < t_end:
+        time.sleep(10)
+        screenWidth, screenHeight = pyautogui.size()    # Macbook pro 13" is 1440x900
+        targetposx = 1200 #screenWidth/2
+        targetposy = 400
+        pyautogui.click(targetposx,targetposy)
+        time.sleep(10)
+        try:
             driver.switch_to.window(driver.window_handles[1])
-            driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        driver.refresh()
-    except IndexError:
-        print("Error")
-        driver.refresh() #if ad doesn't load fast enough-reload the page and start again
-
+            url = driver.current_url
+            url = urlsplit(url)[1]
+            if url in urldict: #counts instances of urls
+                urldict[url] += 1
+            else:
+                urldict[url] = 1
+            while len(driver.window_handles) > 1: #fixes extra tab issue
+                driver.switch_to.window(driver.window_handles[1])
+                driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            driver.refresh()
+        except IndexError as err:
+            print(err)
+            driver.refresh() #if ad doesn't load fast enough-reload the page and start again
+        except Exception as exp:
+            print(exp)
+            driver.refresh() #if ad doesn't load fast enough-reload the page and start again
+except KeyboardInterrupt:
+    pass
 print(urldict)
